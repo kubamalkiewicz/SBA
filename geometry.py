0de82d47538b637981c97422f951ea6413d8bbe0
kubamalkiewicz <kuba.malkiewicz@o2.pl>
2026-05-31 16:42:17 +0200

    Add circle area formula2

--- geometry.py
+++ geometry.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
  print(f"Triangle Area (b=10, h=5): {triangle_area(10, 5)}")
  print(f"Square Area (a=4): {square_area(4)}")
 
- def circle_area(radius):
-"""Calculates the area of a circle.”””
-return math.pi * (radius ** 2)
+def circle_area(radius):
+    """Calculates the area of a circle."""
+    return math.pi * (radius ** 2)
